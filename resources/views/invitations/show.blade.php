4503eea256c8da8e96de951b12db8ac0a8fa3548
fanuel-nashon <fanuelnashon517@gmail.com>
2026-08-26 12:24:27 +0300

    feat: added favicon changes

--- resources/views/invitations/show.blade.php
+++ resources/views/invitations/show.blade.php
@@ -6,6 +6,8 @@
 
         <title>{{ __('Your Invitation') }} — {{ config('app.name') }}</title>
 
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
